395d62dbae2634122dfde616ada05443bb3bdfdf
Topher McCulloch <tophermcculloch@gmail.com>
2018-09-01 15:09:27 -0500

    homepage, header, footer width update

--- footer.php
+++ footer.php
@@ -6,43 +6,33 @@
     
     <div id="footer-container">
     	<div id="footer-wave">&nbsp;</div>
-        <div id="footer-widgets">
+        <div id="footer-widgets" class="container">
         	
             <div class="widget-wrap clearfix">
             	
-                <div class="widget-section">
-                	
-                    <?php /* Widgetised Area */ if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'Footer One' ) ) ?>
-                    
+                <div class="widget-section">                	
+                    <?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'Footer One' ) ) ?> 
                 </div>
                 
-                <div class="widget-section">
-                
-                	<?php /* Widgetised Area */ if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'Footer Two' ) ) ?>
-                    
+                <div class="widget-section">                
+                	<?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'Footer Two' ) ) ?>                    
                 </div>
                 
-                <div class="widget-section">
-                
-                	<?php /* Widgetised Area */ if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'Footer Three' ) ) ?>
-                    
+                <div class="widget-section">                
+                	<?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'Footer Three' ) ) ?>                    
                 </div>
                 
-                <div class="widget-section">
-                
-                	<?php /* Widgetised Area */ if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'Footer Four' ) ) ?>
-                    
+                <div class="widget-section">                
+                	<?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'Footer Four' ) ) ?>
                 </div>
             
             </div>
             
         </div>
     
-        <div id="footer" class="clearfix">
-        
-            <p class="copyright">&copy; Copyright <?php echo date( 'Y' ); ?> <a href="/contact">Topher McCulloch</a></p>            
-            <p class="credit"><?php echo get_theme_mod( 'footer_text' ); ?></p>
-        
+        <div id="footer" class="clearfix container">
+            <p class="copyright">&copy; Copyright <?php echo date( 'Y' ); ?> <a href="/contact">Topher McCulloch</a></p>
+            <p class="credit"><?php echo get_theme_mod( 'footer_text' ); ?></p>        
         </div>	
    	
 	<?php wp_footer(); ?>
